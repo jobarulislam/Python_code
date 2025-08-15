ba7d391399e020ce6dfe633cdb2efa2297f573b1
Md Jobarul Islam <jobarulislam1203@gmail.com>
2025-08-16 05:13:24 +0600

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+#make a dictonary
+print (f"............Welcome to NYC MET MUSEUM.............")
+print (f"ART DETAILS:")
+The_Kangxi_Emperors = {
+        'Artist' : 'Wang Hui',
+        'preiod' : ' Qing dynasty (1644–1911)',
+        'date' : 'datable to 1698'
+        }
+print (The_Kangxi_Emperors)
+
+print (f"...........................................")
+
+print ("Values : ", The_Kangxi_Emperors.values())
+print ("keys : ", The_Kangxi_Emperors.keys())
+print ("items : ", The_Kangxi_Emperors.items())
