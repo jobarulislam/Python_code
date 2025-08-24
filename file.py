4ed0051353e52502b21cf1f19ef4032fdf94eb30
Md Jobarul Islam <jobarulislam1203@gmail.com>
2025-08-25 00:41:57 +0600

    Create file.py

---
+++ file.py
@@ -0,0 +1,9 @@
+#File input/output
+file = open('diarie.txt','w')
+
+file.write("Hello Codedex.io.\n")
+file.write("This a diarie for prctice File I/O control.\n")
+file.write("Today is 25 aug 2025.\n")
+file.write("I was working on Python.\n")
+file.write("This helps me to undrastending code.\n")
+print (f"File diaries.txt is created!")
